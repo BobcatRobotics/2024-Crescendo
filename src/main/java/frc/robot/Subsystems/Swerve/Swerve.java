1635a027301a86cfd714f5b50bfa1663573a17dd
dh28567 <devin.hazelton42@gmail.com>
2024-01-10 07:33:15 -0500

    Updated everything to latest version

--- src/main/java/frc/robot/Subsystems/Swerve/Swerve.java
+++ src/main/java/frc/robot/Subsystems/Swerve/Swerve.java
@@ -56,6 +56,17 @@ public class Swerve extends SubsystemBase {
                         SwerveConstants.maxSpeed,
                         SwerveConstants.driveBaseRadius,
                         new ReplanningConfig(true, true)),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+            
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
                 this);
     }
 
