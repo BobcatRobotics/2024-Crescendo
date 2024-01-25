41b1c346d807cd24190eb278e86b83087412c63b
dh28567 <devin.hazelton42@gmail.com>
2024-01-24 19:20:39 -0500

    Added buildconstants file

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Bobcat-Base-Swerve";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 42;
+  public static final String GIT_SHA = "08f15fab2bfbe1c67f4a6e46f1ae24d4e7491be2";
+  public static final String GIT_DATE = "2024-01-07 17:03:08 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-07 20:46:15 EST";
+  public static final long BUILD_UNIX_TIME = 1704678375685L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
