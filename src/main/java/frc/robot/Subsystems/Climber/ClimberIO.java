44d25b97b8ce1643298970f49fb81c0f1fff0891
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-01-31 19:46:56 -0500

    Skeleton for ClimberIO.java

--- src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
@@ -1,1 +1,40 @@
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+
+public class ClimberSubsystem extends SubsystemBase {
+  public static enum LockState {LOCKED, UNLOCKED};
+  
+  /**
+   * Creates a new subsystem.
+   */
+
+  private final Spark climber;
+  private final Servo lockServo;
+
+  public ClimberSubsystem() {
+      climber = new Spark(Constants.PWMPorts.kClimberMotor);
+      lockServo = new Servo(Constants.PWMPorts.kClimberServo);
+      lock();
+  }
+
+  public void climb(double speed ) {
+    climber.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public void lock() {
+    lockServo.setAngle(140*0.666666);
+  }
+
+  public void unlock() {
+    lockServo.setAngle(0);
+  }
+}
