b4a8d31f67fab4947a4691ebfd8d418de5830191
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-03 11:00:40 -0500

    Started on Climber.java
    It should be fine for now. Build worked. There's more to add after CAD drops the climber.

---
+++ src/main/java/frc/robot/Subsystems/Climber/Climber.java
@@ -0,0 +1,13 @@
+package frc.robot.Subsystems.Climber;
+import frc.robot.Constants;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climber extends SubsystemBase {
+
+    public Climber(ClimberIO ClimbModule){
+
+    }
+}
