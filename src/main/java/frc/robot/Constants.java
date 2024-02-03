6c3268564be59738018fed4365dc84b44e7c1926
zainlok06@gmail.com <zainlok06@gmail.com>
2024-02-03 14:48:33 -0500

    Added motion magic to the angle motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -182,18 +182,53 @@ public class Constants {
         public static final int topMotorID = 0;
         public static final InvertedValue topMotorInvert = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue topMotorBrakeMode = NeutralModeValue.Coast;
+        // public static final int kTopS = 0;
+        // public static final int kTopV = 0;
+        // public static final int kTopA = 0;
+        // public static final int kTopP = 0;
+        // public static final int kTopI = 0;
+        // public static final int kTopD = 0;
+        // public static final double topMotorMotionMagicCruiseVelocity = 0;
+        // public static final double topMotorMotionMagicAcceleration = 0;
+        // public static final double topMotorMotionMagicJerk = 0;
 
         public static final int bottomMotorID = 0;
         public static final InvertedValue bottomMotorInvert = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue bottomMotorBrakeMode = NeutralModeValue.Coast;
+        // public static final int kBottomS = 0;
+        // public static final int kBottomV = 0;
+        // public static final int kBottomA = 0;
+        // public static final int kBottomP = 0;
+        // public static final int kBottomI = 0;
+        // public static final int kBottomD = 0;
+        // public static final double bottomMotorMotionMagicCruiseVelocity = 0;
+        // public static final double bottomMotorMotionMagicAcceleration = 0;
+        // public static final double bottomMotorMotionMagicJerk = 0;
 
         public static final int angleMotorID = 0;
         public static final InvertedValue angleMotorInvert = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue angleMotorBrakeMode = NeutralModeValue.Brake;
+        public static final int kAngleS = 0;
+        public static final int kAngleV = 0;
+        public static final int kAngleA = 0;
+        public static final int kAngleP = 0;
+        public static final int kAngleI = 0;
+        public static final int kAngleD = 0;
+        public static final double angleMotorMotionMagicCruiseVelocity = 0;
+        public static final double angleMotorMotionMagicAcceleration = 0;
+        public static final double angleMotorMotionMagicJerk = 0;
 
         public static final int feederMotorID = 0;
         public static final InvertedValue feederMotorInvert = InvertedValue.Clockwise_Positive;
         public static final NeutralModeValue feederMotorBrakeMode = NeutralModeValue.Coast;
-
+        // public static final int kFeederS = 0;
+        // public static final int kFeederV = 0;
+        // public static final int kFeederA = 0;
+        // public static final int kFeederP = 0;
+        // public static final int kFeederI = 0;
+        // public static final int kFeederD = 0;
+        // public static final double feederMotorMotionMagicCruiseVelocity = 0;
+        // public static final double feederMotorMotionMagicAcceleration = 0;
+        // public static final double feederMotorMotionMagicJerk = 0;
     }
 }
--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIOFalcon.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIOFalcon.java
@@ -4,19 +4,37 @@ package frc.robot.Subsystems.Shooter;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 
 import frc.robot.Constants;
-import frc.robot.Constants.shooterConstants;;
+import frc.robot.Constants.shooterConstants;
 
 public class ShooterIOFalcon implements ShooterIO {
     private TalonFX topMotor;
     private TalonFX bottomMotor;
     private TalonFX angleMotor;
     private TalonFX feederMotor;
+    private final Slot0Configs angleMotor_slot0;
+    private final Slot0Configs topMotor_slot0;
+    private final Slot0Configs bottomMotor_slot0;
+    private final Slot0Configs feederMotor_slot0;
+    private final MotionMagicConfigs angleMotorMotionMagicConfigs;
+    private final MotionMagicConfigs feederMotorMotionMagicConfigs;
+    private final MotionMagicConfigs topMotorMotionMagicConfigs;
+    private final MotionMagicConfigs bottomMotorMotionMagicConfigs;
+    private final MotionMagicVoltage m_request;    
 
     private final DutyCycleOut request; //same as percent out
     
     public ShooterIOFalcon() {
+        m_request = new MotionMagicVoltage(0);
+
+
         topMotor = new TalonFX(Constants.shooterConstants.topMotorID); //initializes TalonFX motor 1
         bottomMotor = new TalonFX(Constants.shooterConstants.bottomMotorID); //initializes TalonFX motor 2
         angleMotor = new TalonFX(Constants.shooterConstants.angleMotorID); //initializes TalonFX motor 3
@@ -26,21 +44,44 @@ public class ShooterIOFalcon implements ShooterIO {
         topMotor.getConfigurator().apply(topConfigs);
         topConfigs.MotorOutput.Inverted = shooterConstants.topMotorInvert;
         topConfigs.MotorOutput.NeutralMode = shooterConstants.topMotorBrakeMode;
+        //Motion Magic configs
+        topMotor_slot0 = topConfigs.Slot0;
+        topMotorMotionMagicConfigs = topConfigs.MotionMagic;
+
 
         TalonFXConfiguration bottomConfigs = new TalonFXConfiguration();
         bottomMotor.getConfigurator().apply(bottomConfigs);
         bottomConfigs.MotorOutput.Inverted = shooterConstants.bottomMotorInvert;
         bottomConfigs.MotorOutput.NeutralMode = shooterConstants.bottomMotorBrakeMode;
+        //Motion Magic configs
+        bottomMotor_slot0 = bottomConfigs.Slot0;
+        bottomMotorMotionMagicConfigs = bottomConfigs.MotionMagic;
 
         TalonFXConfiguration angleConfigs = new TalonFXConfiguration();
         angleMotor.getConfigurator().apply(angleConfigs);
         angleConfigs.MotorOutput.Inverted = shooterConstants.angleMotorInvert;
         angleConfigs.MotorOutput.NeutralMode = shooterConstants.angleMotorBrakeMode;
+        //Motion Magic configs
+        angleMotor_slot0 = angleConfigs.Slot0;
+        angleMotorMotionMagicConfigs = angleConfigs.MotionMagic;
+        angleMotor_slot0.kS = Constants.shooterConstants.kAngleS;
+        angleMotor_slot0.kV = Constants.shooterConstants.kAngleV;
+        angleMotor_slot0.kA = Constants.shooterConstants.kAngleA;
+        angleMotor_slot0.kP = Constants.shooterConstants.kAngleP;
+        angleMotor_slot0.kI = Constants.shooterConstants.kAngleI;
+        angleMotor_slot0.kD = Constants.shooterConstants.kAngleD;
+        angleMotorMotionMagicConfigs.MotionMagicCruiseVelocity = Constants.shooterConstants.angleMotorMotionMagicCruiseVelocity;
+        angleMotorMotionMagicConfigs.MotionMagicAcceleration = Constants.shooterConstants.angleMotorMotionMagicAcceleration;
+        angleMotorMotionMagicConfigs.MotionMagicJerk = Constants.shooterConstants.angleMotorMotionMagicJerk;
 
         TalonFXConfiguration feederConfigs = new TalonFXConfiguration();
         feederMotor.getConfigurator().apply(feederConfigs);
         feederConfigs.MotorOutput.Inverted = shooterConstants.feederMotorInvert;
         feederConfigs.MotorOutput.NeutralMode = shooterConstants.feederMotorBrakeMode;
+        //Motion Magic configs
+        feederMotor_slot0 = feederConfigs.Slot0;
+        feederMotorMotionMagicConfigs = feederConfigs.MotionMagic;
+
 
         request = new DutyCycleOut(0).withEnableFOC(true);
     }
