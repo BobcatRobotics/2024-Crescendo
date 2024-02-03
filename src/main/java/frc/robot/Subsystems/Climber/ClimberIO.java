c2e8456eb4cb3839ed92d3f8f89342d8968fc6fc
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-03 10:54:41 -0500

    Added some functions and constants to ClimberIO.java

--- src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
@@ -5,13 +5,22 @@ import org.littletonrobotics.junction.AutoLog;
 import java.lang.Object;
 import edu.wpi.first.wpilibj.MotorSafety;
 import com.ctre.phoenix6.wpiutils.MotorSafetyImplem;
+import com.ctre.phoenix6.jni.CtreJniWrapper;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.core.CoreTalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 
 
 public interface ClimberIO {
     @AutoLog
     public static class ClimberIOInputs{
+        public double climberPosition = 0.0;
+        public double climberEncoderValue = 0.0;
+    }
 
+    public default double getClimberPosition(int climberEncoderValue){
+        return 0.0;
     }
 
     public default void updateClimberPosition(){
@@ -23,7 +32,7 @@ public interface ClimberIO {
     }
 
     public default void retractClimber(){
-        
+
     }
 
 
