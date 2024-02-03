e72246683cfc6c87e5b0dc75f106ab1c7f037a3d
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-03 10:46:27 -0500

    added some functions

--- src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
+++ src/main/java/frc/robot/Subsystems/Climber/ClimberIO.java
@@ -1,11 +1,30 @@
-package frc.robot.Subsystems.Swerve;
+package frc.robot.Subsystems.Climber;
 
 import org.littletonrobotics.junction.AutoLog;
 
+import java.lang.Object;
+import edu.wpi.first.wpilibj.MotorSafety;
+import com.ctre.phoenix6.wpiutils.MotorSafetyImplem;
 
 
 
 public interface ClimberIO {
     @AutoLog
+    public static class ClimberIOInputs{
+
+    }
+
+    public default void updateClimberPosition(){
+
+    }
+
+    public default void deployClimber(){
+
+    }
+
+    public default void retractClimber(){
+        
+    }
+
 
 }
