5a97842936346eaf73ab9321e6a5e21603c6fc27
dh28567 <devin.hazelton42@gmail.com>
2024-02-12 12:25:02 -0500

    Fixed file names and removed unused file

--- src/main/java/frc/robot/Commands/Swerve/GrabNote.java
+++ src/main/java/frc/robot/Commands/Swerve/GrabNote.java
@@ -2,21 +2,21 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Commands;
+package frc.robot.Commands.Swerve;
 
-import java.util.Optional;
+import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.apriltag.AprilTag;
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.LimelightHelpers;
 import frc.robot.Subsystems.Swerve.Swerve;
 import frc.robot.Subsystems.Vision.Vision;
 
 
-public class AlignToTag extends Command {
+public class GrabNote extends Command {
   /** Creates a new grabNote. */
 
   private Vision vision;
@@ -27,17 +27,13 @@ public class AlignToTag extends Command {
   private PIDController xController;
   private PIDController yController;
   private PIDController thetaController;
-  private Translation2d dist;
-  private Optional<Integer> tagID;
-  private int currID;
-  private AprilTag currTag;
-  
+  private Pose2d notePos;
 
-  public AlignToTag(Swerve swerve, Vision vision, Optional<Integer> tagID) {
+  public GrabNote(Swerve swerve, Vision vision) {
     this.swerve = swerve;
-    this.vision = vision;
-    this.tagID = tagID;
     addRequirements(swerve);
+    this.vision = vision;
+
   }
 
   // Called when the command is initially scheduled.
@@ -47,7 +43,6 @@ public class AlignToTag extends Command {
     xController.setTolerance(0.4);
     yController = new PIDController(kP, 0,0);
     yController.setTolerance(0.4);
-    yController.setSetpoint(1);
     thetaController = new PIDController(kPRotation, 0,0);
     thetaController.setTolerance(4);
   }
@@ -55,19 +50,17 @@ public class AlignToTag extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(vision.getTV()){
-    currID = (int) LimelightHelpers.getFiducialID(null);
-    }
 
-
-    if(tagID != null && tagID.get() == currID){
-      swerve.drive(new Translation2d(), -thetaController.calculate(vision.getTX().getDegrees()), false, false);
-    }else{
-      swerve.drive(thetaController.getPositionError() < 4? new Translation2d(0,yController.calculate(vision.getTranslationToTag(currID).getX())):new Translation2d(), -thetaController.calculate(vision.getTX().getDegrees()), false, false);
-    }
-    
+  if (vision.getTClass()==0){
+    //if were more than 5 degrees off, only rotate, once were within 5 degrees, translate.
+    notePos = vision.getNotePose();
+     if(!(Math.abs(thetaController.getPositionError()) < 5)){
+     swerve.drive(new Translation2d(), -thetaController.calculate(notePos.getRotation().getDegrees()),false,false);
+     }else{
+      swerve.drive(new Translation2d(0,xController.calculate(notePos.getX())), -thetaController.calculate(notePos.getRotation().getDegrees()),false,false);
+     }
   }
-    
+}
 
   // Called once the command ends or is interrupted.
   @Override
