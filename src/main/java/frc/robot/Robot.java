0f0901c79c11c4f7a555ae60fc261b7b3f32b83f
Arnav Thakrar <arnav.thakrar@gmail.com>
2024-02-15 18:44:00 -0500

    commented out logging, fixed rpm calculation math error [all]

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,7 +69,7 @@ public class Robot extends LoggedRobot {
  
 
     // Logger.disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
-    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+    //Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
 
     m_robotContainer = new RobotContainer();
   }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -155,7 +155,7 @@ public class RobotContainer {
     gp.button(9).whileTrue(new InstantCommand(m_intake::runOut)).onFalse(new InstantCommand(m_intake::stop)); // start
 
     /* Shooter Controls */
-    gp.button(5).whileTrue(new InstantCommand(() -> m_shooter.setSpeed(300*60))).onFalse(new InstantCommand(m_shooter::stop)); // left bumper
+    gp.button(5).whileTrue(new InstantCommand(() -> m_shooter.setSpeed(1000/60))).onFalse(new InstantCommand(m_shooter::stop)); // left bumper
     //gp.button(2).whileTrue(new InstantCommand(() -> m_shooter.setVelocityTune(SmartDashboard.getNumber("ShooterRPM", 0)))).onFalse(new InstantCommand(() -> m_shooter.stop()));
     gp.button(2).onTrue(new InstantCommand(() -> m_shooter.setPercentOut(0.2)).withTimeout(1)).onFalse(new InstantCommand(() -> m_shooter.stopAngle()));
     /* Drive with gamepad */
